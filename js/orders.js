d118f36133bdd5452970186a2e727c6432ada84f
Ruan Augusto Bernardi Pasa <principalruanbernardi@gmail.com>
2024-10-16 08:42:04 -0300

    Initial commit

---
+++ js/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'iPhone xr 64gb',
+        productNumber: '85631',
+        paymentStatus: 'Devendo',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Lavander KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Reembolsado',
+        shipping: 'Cancelado'
+    },
+    {
+        productName: 'Sansumg S24 Ultra',
+        productNumber: '49347',
+        paymentStatus: 'Devendo',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'PS4 + 500 games',
+        productNumber: '69699',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '78902',
+        paymentStatus: 'Pago',
+        shipping: 'Pendente'
+    }
+]
